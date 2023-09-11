12da32041ec73fbec80529a70ec6656f9e11d68a
minhquoc3908 <nguyenquoc14076@gmail.com>
2023-09-11 09:27:36 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,10 @@
+// inputWidth=prompt("Enter the width");
+// inputHeight=prompt("Enter the Height");
+// let width=parseInt(inputWidth);
+// let height= parseInt(inputHeight);
+// let area = width*height;
+// document.write("the area is "+area);
+let inputWidth =prompt(parseInt("Enter the width"));
+let inputHeight=prompt(parseInt("Enter your Height"));
+let area=inputHeight*inputWidth
+document.write("your are :"+area);
